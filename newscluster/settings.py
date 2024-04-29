14f02eb46fcff08f212ce5b7dd3b30a03dac16f6
Kevin Gopito <119961345+DSevin@users.noreply.github.com>
2024-04-29 18:52:20 +0200

    Update settings.py
    yees

--- newscluster/settings.py
+++ newscluster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5mgm^8w6@mq0#@-mcojuf+5*njav3zeep&r$fb(e!a!fb+9%^b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.com']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
